748e787f3d3443684cd0a9d9b9d5fa8282b57f62
hayongwoon <a01046641857@gmail.com>
2022-02-14 19:18:33 +0900

    use github action

--- sparta/urls.py
+++ sparta/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from typing import Dict
+
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
 from ninja import NinjaAPI
-
-from typing import Dict
-from django.http import HttpRequest
 
 api = NinjaAPI()
 
